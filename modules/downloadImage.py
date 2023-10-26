e3274064cb3501bc2865cca935328ce93cee7b46
kangzai228 <kangzai228@163.com>
2023-10-26 08:50:04 +0800

    下载图片

--- modules/downloadImage.py
+++ modules/downloadImage.py
@@ -1,4 +1,11 @@
-import re
+import re,requests,os
+
+def savefile(fileName,content):
+    file_path ='/'.join(fileName.split('/')[:-1])
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+    with open(fileName,'wb') as f:
+        f.write(content)
 
 def downloadImage(param):
     reg='<img src=\\"(.*?)\\" />'
@@ -6,4 +13,19 @@ def downloadImage(param):
     res_list=re.findall(reg,param)
     for res in res_list:
         url='https://xkjk.jxeea.cn:8000'+res
-        print(url)
+        # print(url)
+        headers = {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+        'Origin': 'https://xkjk.jxeea.cn:8000',
+        'Referer': 'https://xkjk.jxeea.cn:8000/',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+        }
+        r=requests.get(url,headers=headers)
+        if r.status_code==200:
+            savefile('.'+res,r.content)
+            print(url,'图片保存成功')
+        else:
+            print(url,'图片保存失败')
+
+
